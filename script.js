94d9e7922e143350bf6375a9d2342e5d2680b669
ms-online <27732357@qq.com>
2020-03-21 19:49:15 +0800

    lesson-2

---
+++ script.js
@@ -0,0 +1,9 @@
+// 获取节点
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+
+// 事件监听
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
+closeBtn.addEventListener("click", () => rules.classList.remove("show"));
