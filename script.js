7d625515b7e3871afa40dd5967661c20a0c9cafb
ms-online <27732357@qq.com>
2020-03-21 20:51:04 +0800

    lesson-4

--- script.js
+++ script.js
@@ -4,6 +4,61 @@ const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
 const canvas = document.getElementById("canvas");
 
+const ctx = canvas.getContext("2d");
+
+let score = 0;
+
+// 创建撞击球
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4
+};
+
+// 创建挡板
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0
+};
+
+// 绘制撞击球
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// 绘制挡板
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// 绘制得分
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillText(`得分：${score}`, canvas.width - 100, 30);
+}
+
+// 所有绘制函数
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+draw();
 // 事件监听
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
