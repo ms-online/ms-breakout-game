40a7d590a4c172d454078df9db185be9877e143f
ms-online <27732357@qq.com>
2020-03-22 10:52:44 +0800

    lesson-6

--- script.js
+++ script.js
@@ -90,14 +90,68 @@ function drawScore() {
   ctx.fillText(`得分：${score}`, canvas.width - 100, 30);
 }
 
+// 动画函数
+// 移动挡板动画
+function movePaddle() {
+  paddle.x += paddle.dx;
+  // console.log(paddle.x);
+
+  // 设置边界
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
 // 所有绘制函数
 function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
   drawScore();
   drawBricks();
 }
-draw();
+
+// 创建update函数，更新所有绘制函数和动画
+function update() {
+  // 动画函数
+  movePaddle();
+
+  // 所有的绘制函数
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+// 键盘函数
+function keyDown(e) {
+  if (e.key === "ArrowRight" || e.key === "Right") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "ArrowLeft" || e.key === "Left") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+function keyup(e) {
+  if (
+    e.key === "ArrowRight" ||
+    e.key === "Right" ||
+    e.key === "ArrowLeft" ||
+    e.key === "Left"
+  ) {
+    paddle.dx = 0;
+  }
+}
+
 // 事件监听
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyup);
+
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
